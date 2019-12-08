83fbf35bddb28fe16177c7f3c32473e5b268a30a
toa0904 <pazutomo0428@gmail.com>
2019-12-08 20:10:35 +0900

    修正

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -8,7 +8,7 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    Message.create(content: data['message'])
+    Message.create！(content: data['message'])
     ActionCable.server.broadcast 'room_channel', data['message']
   end
 end
