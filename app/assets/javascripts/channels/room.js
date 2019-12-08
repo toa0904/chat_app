a521095d47eac2fff4a50ec9d3b5369366500f2c
toa0904 <pazutomo0428@gmail.com>
2019-12-08 21:00:18 +0900

    js追記

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,5 +1,6 @@
 App.room = App.cable.subscriptions.create("RoomChannel", {
   connected: function() {
+    console.log('connected')
     // Called when the subscription is ready for use on the server
   },
 
@@ -7,12 +8,23 @@ App.room = App.cable.subscriptions.create("RoomChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function(data) {
-    alert(data)
+  received: function(message) {
+    const messages = document.getElementById('messages')
+    messages.innerHTML += `<p>${message}</p>`
     // Called when there's incoming data on the websocket for this channel
   },
 
-  speak: function() {
-    return this.perform('speak', {message: 'aaaaaaa'});
+  speak: function(content) {
+    return this.perform('speak', {message: content});
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const input = document.getElementById('chat-input')
+  const button = document.getElementById('button')
+  button.addEventListener('click', function() {
+    const content = input.value
+    App.room.speak(content)
+    input.value = ''
+  })
+})
